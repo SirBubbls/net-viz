d6823d46ea5576c391371b6c8d307ce2b37a0542
SirBubbls <lucassas@live.de>
2020-03-08 12:35:22 +0100

    edges prototype

--- src/app/graph/graph.component.ts
+++ src/app/graph/graph.component.ts
@@ -9,41 +9,57 @@ import sigma from 'sigma';
 })
 export class GraphComponent implements OnInit {
   graph: sigma;
+  properties: {
+    name: string,
+    count: number
+  }[] = [];
 
 
   constructor() { }
 
 
-  private addLayer(count: any, layer: any, name: string, previous?: string) {
+  private async addLayer(count: number, layer: number, name: string, previous?: string) {
+    this.properties.push({
+      name: name,
+      count: count
+    });
+
     for (let i = 0; i < count; i++) {
       this.graph.graph.addNode({
         id: `${name}-${i}`,
         x: i / 2,
         y: layer,
         label: `${name}-${i}`,
-        size: 4
+        size: 1
       });
+
       if (!previous) { continue; }
 
-      for (let j = 0; j < 5; j ++) {
-        this.graph.graph.addEdge({
-          id: `${previous}${j}-${name}${i}`,
-          source: `${previous}-${j}`,
-          target: `${name}-${i}`
-        });
-      }
+      // for (let j = 0; j < this.properties.reverse()[1].count; j ++) {
+      //   this.graph.graph.addEdge({
+      //     id: `${previous}${j}-${name}${i}`,
+      //     source: `${previous}-${j}`,
+      //     target: `${name}-${i}`
+      //   });
+      // }
     }
     this.graph.refresh();
   }
 
-  ngOnInit(): void {
+ngOnInit(): void {
     this.graph = new sigma('container');
     this.graph.settings({
       edgeColor: 'default',
-      defaultEdgeColor: '#999'
+      defaultEdgeColor: '#999',
+      batchEdgesDrawing: true,
+      sideMargin: 1,
+      canvasEdgesBatchSize: 5,
+      edgeHoverSizeRatio: 2
     });
+
     this.addLayer(5, 0, 'input');
     this.addLayer(10, 1, 'dense', 'input');
+    this.addLayer(10, 2, 'dense_2', 'dense');
+    console.log(this.graph.graph.nodes());
   }
-
 }
